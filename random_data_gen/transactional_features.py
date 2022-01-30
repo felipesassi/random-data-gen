f1a007776402440bb530d63eead6f146a02e41bb
Felipe Sassi <felipesassi@outlook.com>
2022-01-30 17:26:06 -0300

    rename files

---
+++ random_data_gen/transactional_features.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+def generate_create_date_array(n_rows: int, start_date: str, end_date: str) -> list:
+
+    return list(pd.date_range(start=start_date, end=end_date, periods=n_rows))
+
+
+def generate_consumer_ids(n_rows: int, n_ids: int) -> list:
+
+    consumer_ids = range(1, n_ids + 1)
+    return list(np.random.choice(consumer_ids, n_rows))
+
+
+def generate_transaction_spend(
+    n_rows: int, mean_spend: float, std_spend: float
+) -> list:
+
+    return list(np.random.normal(mean_spend, std_spend, n_rows))
--- random_data_gen/transactional_generator.py
+++ random_data_gen/transactional_generator.py
@@ -1,20 +1,43 @@
 import pandas as pd
-import numpy as np
+from random_data_gen.transactional_features import (
+    generate_create_date_array,
+    generate_consumer_ids,
+    generate_transaction_spend,
+)
 
 
-def generate_create_date_array(n_rows: int, start_date: str, end_date: str) -> list:
+class DataGenerator:
+    def __init__(
+        self,
+        n_rows: int,
+        n_ids: int,
+        mean_spend: float,
+        std_spend: float,
+        start_date: str,
+        end_date: str,
+    ) -> None:
 
-    return list(pd.date_range(start=start_date, end=end_date, periods=n_rows))
+        self.n_rows = n_rows
+        self.n_ids = n_ids
+        self.mean_spend = mean_spend
+        self.std_spend = std_spend
+        self.start_date = start_date
+        self.end_date = end_date
 
+    def __call__(self):
 
-def generate_consumer_ids(n_rows: int, n_ids: int) -> list:
+        date_list = generate_create_date_array(
+            self.n_rows, self.start_date, self.end_date
+        )
+        consumer_list = generate_consumer_ids(self.n_rows, self.n_ids)
+        transactional_spend_list = generate_transaction_spend(
+            self.n_rows, self.mean_spend, self.std_spend
+        )
 
-    consumer_ids = range(1, n_ids + 1)
-    return list(np.random.choice(consumer_ids, n_rows))
-
-
-def generate_transaction_spend(
-    n_rows: int, mean_spend: float, std_spend: float
-) -> list:
-
-    return list(np.random.normal(mean_spend, std_spend, n_rows))
+        return pd.DataFrame(
+            data={
+                "consumer_id": consumer_list,
+                "created_at": date_list,
+                "payment_value": transactional_spend_list,
+            }
+        )
